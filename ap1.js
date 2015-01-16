0296a02d1b644bb2ecd3aebc13f4c5357fcce1f2
danben4 <danielbenediktsson4@gmail.com>
2015-01-16 21:09:12 +0000

    Updated the scripts

--- ap1.js
+++ ap1.js
@@ -1,70 +1,54 @@
-/* © 2009 ROBO Design
- * http://www.robodesign.ro
- */
-
-// Keep everything in anonymous function, called on window load.
 if(window.addEventListener) {
-window.addEventListener('load', function () {
-  var canvas, context;
+  window.addEventListener('load', function() {
+    var canvas, canvasContext, tool;
 
-  // Initialization sequence.
-  function init () {
-    // Find the canvas element.
-    canvas = document.getElementById('canvasDrawBoard');
-    
-    if (!canvas) {
-      alert('Error: I cannot find the canvas element!');
-      return;
+    function init() {
+      canvas = document.getElementById('drawBoard');
+
+      if(!canvas) {
+        alert('The canvas element cannot be found!');
+        return;
+      } else if(!canvas.getContext) {
+        alert('The context in the canvas element cannot be found!');
+        return;
+      }
+
+      canvasContext = canvas.getContext('2d');
+      canvasContext.canvas.width = window.innerWidth - 20;
+      canvasContext.canvas.height = window.innerHeight - 20;
+
+      if(!canvasContext) {
+        alert('The canvas context cannot be reached!');
+        return;
+      }
+
+      //tool = new pen();
+
+      canvas.addEventListener('mousemove', ev_mousemove, false);
     }
 
-    if (!canvas.getContext) {
-      alert('Error: no canvas.getContext!');
-      return;
+    var started = false;
+
+    function ev_mousemove(ev) {
+      var x, y;
+
+      // get the mouse position relative to the canvas element
+      if (ev.layerX || ev.layerX == 0) {
+        x = ev.layerX;
+        y = ev.layerY;
+      }
+
+      if(!started) {
+        canvasContext.beginPath();
+        canvasContext.moveTo(x, y);
+        started = true;
+      } else {
+        canvasContext.lineTo(x, y);
+        canvasContext.stroke();
+      }
     }
 
-    // Get the 2D canvas context.
-    context = canvas.getContext('2d');
-    context.canvas.width = window.innerWidth - 20;
-    context.canvas.height = window.innerHeight - 20;
-    //context.fillStyle="#D0D0D0 ";
-    //context.fillRect(0,0,context.canvas.width,context.canvas.height);
-    
-    if (!context) {
-      alert('Error: failed to getContext!');
-      return;
-    }
+    init();
 
-    // Attach the mousemove event handler.
-    canvas.addEventListener('mousemove', ev_mousemove, false);
-  }
-
-  // The mousemove event handler.
-  var started = false;
-  function ev_mousemove (ev) {
-    var x, y;
-
-    // Get the mouse position relative to the canvas element.
-    if (ev.layerX || ev.layerX == 0) { // Firefox
-      x = ev.layerX;
-      y = ev.layerY;
-    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
-      x = ev.offsetX;
-      y = ev.offsetY;
-    }
-
-    // The event handler works like a drawing pencil which tracks the mouse 
-    // movements. We start drawing a path made up of lines.
-    if (!started) {
-      context.beginPath();
-      context.moveTo(x, y);
-      started = true;
-    } else {
-      context.lineTo(x, y);
-      context.stroke();
-    }
-  }
-
-  init();
-}, false); }
-
-// vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
+  }, false);
+}
