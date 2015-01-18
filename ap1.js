2a5617486a09bac1ded142f91266e566e55bd31e
danben4 <danielbenediktsson4@gmail.com>
2015-01-18 14:43:10 +0000

    alot of fixing

--- ap1.js
+++ ap1.js
@@ -1,84 +1,75 @@
-if(window.addEventListener) {
-  window.addEventListener('load', function() {
+$(document).ready(function() {
+  var canvas = document.getElementById('drawBoard');
+  var context = canvas.getContext();
 
-    var canvas, canvasContext;
-
-    var drawing = {
-      //shapes: [],
-      nextObject: 'pen',
-      nextColor: 'black',
-
-      /*
-      drawAll: function drawAll() {
-        for(int i = 0; i < shapes.length; i++) {
-          shapes[i].draw();
-        }
-      }*/
-    };
-
-    function init() {
-      canvas = document.getElementById('drawBoard');
-
-      if(!canvas) {
-        alert('The canvas element cannot be found!');
-        return;
-      } else if(!canvas.getContext) {
-        alert('The context in the canvas element cannot be found!');
-        return;
-      }
-
-      canvasContext = canvas.getContext('2d');
-      canvasContext.canvas.width = window.innerWidth - 20;
-      canvasContext.canvas.height = window.innerHeight - 20;
-
-      if(!canvasContext) {
-        alert('The canvas context cannot be reached!');
-        return;
-      }
-
-      canvas.addEventListener('mousedown', ev_mousedown, false);
-      canvas.addEventListener('mouseup', ev_mouseup, false);
-    }
-
-    function Pen() {
-      this.draw = function draw(x, y) {
-        //console.log(x, y);
-        canvasContext.lineTo(x,y);
-        canvasContext.stroke();
+  var drawing = {
+    shapes: [],
+    nextObject: 'line',
+    nextColor: 'black',
+    
+    drawAll: function drawAll() {
+      for(var i = 0; i < this.shapes.length; i++) {
+        this.shapes[i].draw();
       }
     }
+  }
 
-    function ev_mousedown(ev) {
-      //console.log("mousedown");
-      canvasContext.beginPath();
-      canvasContext.moveTo(ev._x, ev._y);
-      canvas.addEventListener('mousemove', ev_mousemove, false);
+  function Point(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  function Line(startPoint, endPoint) {
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+
+    this.draw = function draw() {
+      context.beginPath();
+      context.moveTo(this.startPoint.x, this.startPoint.y);
+      context.lineTo(this.endPoint.x, this.endPoint.y);
+      context.stroke();
     }
+  }
 
-    function ev_mouseup(ev) {
-      //console.log("mouseup");
-      canvas.removeEventListener('mousemove', ev_mousemove, false);
+  context = canvas.getContext('2d');
+    context.canvas.width = window.innerWidth - 20;
+    context.canvas.height = window.innerHeight - 20;
+
+    var startX, startY;
+    var isDrawing = false;
+    var startPoint, endPoint;
+
+  $('#drawBoard').mousedown(function(e) {
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+    startPoint = new Point(x, y);
+    isDrawing = true;
+  });
+
+  $('#drawBoard').mousemove(function(e) {
+    if(isDrawing) {
+      var x = e.pageX - this.offsetLeft;
+      var y = e.pageY - this.offsetTop;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.beginPath();
+      context.moveTo(startPoint.x, startPoint.y);
+      context.lineTo(x, y);
+      context.stroke();
+      drawing.drawAll();
     }
+  });
 
-    function ev_mousemove(ev) {
-      //console.log("mousemove");
-      var x, y;
+  $('#drawBoard').mouseup(function(e) {
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+    var endPoint = new Point(x, y);
+    drawing.shapes.push(new Line(startPoint, endPoint));
+    drawing.drawAll();
+    isDrawing = false;
+  });
 
-      // get the mouse position relative to the canvas element
-      if (ev.layerX || ev.layerX == 0) {
-        x = ev.layerX;
-        y = ev.layerY;
-      }
 
-      //console.log(drawing.nextObject);
 
-      if(drawing.nextObject === 'pen') {
-        var pen = new Pen();
-        pen.draw(x, y);
-      }
-    }
 
-    init();
 
-  }, false);
-}
+});
